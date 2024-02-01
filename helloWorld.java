ccba437a3239edc227f81c672a536d0792ef412b
ucflumm <ucflumm@gmail.com>
2024-02-01 13:02:30 +1100

    init

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    public class HelloWorld2 {
+        private String name = "It works";
+    }
+    p[]
+
+}
